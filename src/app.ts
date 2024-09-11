4b57760a9a87ef21f715b9afdb90b53dfb0c9ca7
Hannes Rüger <hannes@rueger.events>
2024-09-11 12:24:47 +0200

    feat: /docs endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-// src/app.ts
-import express, { json, urlencoded } from "express";
 import { RegisterRoutes } from "../build/routes";
+import swaggerUi from "swagger-ui-express";
+import express, { json, urlencoded, Response as ExResponse, Request as ExRequest } from "express";
 
 export const app = express();
 
@@ -11,5 +11,9 @@ app.use(
     })
 );
 app.use(json());
+
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+    return res.send(swaggerUi.generateHTML(await import("../build/swagger.json")));
+});
 
 RegisterRoutes(app);
